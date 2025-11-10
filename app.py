39fa279ced9430a69e6cb2880da9f7a3c9126d56
abeeralzaben3 <abeeralzaben206@gmail.com>
2025-11-10 15:04:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("cv.html")
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3003, debug=True)
